c2a517228d43f341cd64c4cdb0978cacf285993b
Daniel <droque123@gmail.com>
2020-04-03 16:07:35 +0300

    Show MenuItems on Menu

--- src/components/PageContainer/index.js
+++ src/components/PageContainer/index.js
@@ -4,14 +4,13 @@ import PropTypes from 'prop-types';
 import Menu from "./Menu";
 import Page from "./Page";
 
-
 class PageContainer extends React.Component {
 
     render() {
-        const {currentPage, pages, setPage} = this.props;
+        const {currentPage, pages, selectPage} = this.props;
         return <div className={"page-container"}>
             <div className="page-menu">
-                <Menu pages={pages} currentPage={currentPage} selectPage={setPage}/>
+                <Menu pages={pages} currentPage={currentPage} selectPage={selectPage}/>
             </div>
             <Page currentPage={currentPage}/>
         </div>
