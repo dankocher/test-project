b1102ba82c67f4f8ba1fa9554b06bcaafd85e384
Daniel <droque123@gmail.com>
2020-04-03 17:42:25 +0300

    Updates

---
+++ precache-manifest.5fb4f71a52b65da78d55a8d8206a3f22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43055237ce1e6c155dc58e087490ea3a",
+    "url": "/test-project/index.html"
+  },
+  {
+    "revision": "77bf106cba74a938e456",
+    "url": "/test-project/static/css/main.c8411798.chunk.css"
+  },
+  {
+    "revision": "7c7b576fcd43e80e09dd",
+    "url": "/test-project/static/js/2.315e9335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-project/static/js/2.315e9335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77bf106cba74a938e456",
+    "url": "/test-project/static/js/main.c6cd874c.chunk.js"
+  },
+  {
+    "revision": "0ad230646395b131ddf1",
+    "url": "/test-project/static/js/runtime-main.a603ef7e.js"
+  },
+  {
+    "revision": "0092e9206572a7fa11baa0f5d6232efb",
+    "url": "/test-project/static/media/logo.0092e920.svg"
+  }
+]);
