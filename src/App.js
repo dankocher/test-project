58b2f35a87f08ef443a7af7c2af2c4c2acc0b763
Daniel <droque123@gmail.com>
2020-04-03 15:03:46 +0300

    Added pages object and current page to state

--- src/App.js
+++ src/App.js
@@ -4,15 +4,27 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 
 class App extends React.Component {
+
+  state = {
+    currentPage: "home",
+    pages: ["Page 1", "Page 2", "Page 3", "Page 4"]
+  };
+
   render() {
+    const {pages, currentPage} = this.state;
     return <div className="App">
       <div className="app-header">
         <Header selectedPage={currentPage} onClick={() => this.setState({currentPage: 'home'})}/>
       </div>
 
-        <div className="app-content">
-
-        </div>
+      <div className="app-content">
+        {
+          currentPage === "home" ?
+              <Home selectPage={page => this.setState({currentPage: page})}/>
+              :
+              <div>{currentPage}</div>
+        }
+      </div>
     </div>
   }
 }
