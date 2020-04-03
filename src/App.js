32eddf65254ef6939b65cd5af1fc96e7810dfa2a
Daniel <droque123@gmail.com>
2020-04-03 15:03:17 +0300

    Added Home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Header from "./components/Header";
-
+import Home from "./components/Home";
 
 class App extends React.Component {
   render() {
     return <div className="App">
-        <div className="app-header">
-            <Header/>
-        </div>
+      <div className="app-header">
+        <Header selectedPage={currentPage} onClick={() => this.setState({currentPage: 'home'})}/>
+      </div>
 
         <div className="app-content">
 
