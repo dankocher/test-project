85449a1455af467419282eda9244cbcb918d3537
Denis <fafterty@gmail.com>
2020-04-03 16:55:25 +0300

    add build

---
+++ build/precache-manifest.b8f81731741f1acf07c8648564612af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59bda0facf12a0f6518ea84535c89287",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae1488c1b11ca17f1bd3",
+    "url": "/static/css/main.c8411798.chunk.css"
+  },
+  {
+    "revision": "7c7b576fcd43e80e09dd",
+    "url": "/static/js/2.315e9335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.315e9335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1488c1b11ca17f1bd3",
+    "url": "/static/js/main.9224d1c1.chunk.js"
+  },
+  {
+    "revision": "eafb66cdd8dd26081ad2",
+    "url": "/static/js/runtime-main.8099dc88.js"
+  },
+  {
+    "revision": "0092e9206572a7fa11baa0f5d6232efb",
+    "url": "/static/media/logo.0092e920.svg"
+  }
+]);
