caf204c81dccdbfb218cbae4e6987335712422da
Daniel <droque123@gmail.com>
2020-04-03 15:26:39 +0300

    Finished Home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.scss';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import PageContainer from "./components/PageContainer";
 
 class App extends React.Component {
 
   state = {
     currentPage: "home",
-    pages: ["Page 1", "Page 2", "Page 3", "Page 4"]
+    // pages: ["Page 1", "Page 2", "Page 3", "Page 4"],
+    pages: ["Page 1", "Page 2", "Page 3", "Page 4", "Page 5", "Page 6", "Page 7", "Page 8", "Page 9"]
   };
 
   render() {
@@ -20,9 +22,9 @@ class App extends React.Component {
       <div className="app-content">
         {
           currentPage === "home" ?
-              <Home selectPage={page => this.setState({currentPage: page})}/>
+              <Home selectPage={page => this.setState({currentPage: page})} pages={pages}/>
               :
-              <div>{currentPage}</div>
+              <PageContainer pages={pages} currentPage={currentPage}/>
         }
       </div>
     </div>
