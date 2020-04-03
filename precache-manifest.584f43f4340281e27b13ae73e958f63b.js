7da174e11c9e5900210dc92edb161a5d6c06b114
Daniel <droque123@gmail.com>
2020-04-03 17:28:12 +0300

    Updates

---
+++ precache-manifest.584f43f4340281e27b13ae73e958f63b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73fbdba78b8e36c7fa4f5ea2a569b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94de1b8d3a274637d471",
+    "url": "/static/css/main.c8411798.chunk.css"
+  },
+  {
+    "revision": "7c7b576fcd43e80e09dd",
+    "url": "/static/js/2.315e9335.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.315e9335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94de1b8d3a274637d471",
+    "url": "/static/js/main.23491e29.chunk.js"
+  },
+  {
+    "revision": "eafb66cdd8dd26081ad2",
+    "url": "/static/js/runtime-main.8099dc88.js"
+  },
+  {
+    "revision": "0092e9206572a7fa11baa0f5d6232efb",
+    "url": "/static/media/logo.0092e920.svg"
+  }
+]);
